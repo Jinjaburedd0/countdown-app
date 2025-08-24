99e046ba6bb71e496626f082106d20eca7d0fb28
Jinjaburedd0 <aiongamefan@gmail.com>
2025-08-24 19:33:06 +0200

    Initial commit

---
+++ countdown-app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  targetDate: new Date('2023-12-31T23:59:59Z') // Set your target date here
+};
---
+++ countdown-app/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  targetDate: new Date('2024-12-31T23:59:59Z') // Set your target date here
+};
